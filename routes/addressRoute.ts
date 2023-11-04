f28edf9b2a1ab9c193b5ffbbb12a9b8ea5fd0d61
poonam-kumawat <mailpoonam2002@gmail.com>
2023-11-04 23:54:21 +0530

    added retrive api

--- routes/addressRoute.ts
+++ routes/addressRoute.ts
@@ -9,7 +9,6 @@ addressRouter
     try {
       ///user id will be taken by token
       const { email, name, address, phoneNumber } = req.body;
-      console.log(req.body);
       const createAddress = {
         email: email,
         name: name,
@@ -18,6 +17,18 @@ addressRouter
       };
       const insertedAddresses = await addressSchema.create(createAddress);
       return res.status(201).json(insertedAddresses);
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+addressRouter
+  .route("/retrive-details")
+  .post(async (req: Request, res: Response) => {
+    try {
+      const filter = req.body;
+      const data = await addressSchema.find(filter);
+      return res.status(201).json(data);
     } catch (error) {
       return res.status(500).json({ error: "Internal Server Error" });
     }
