e60073682d015adf4da9039efc4eccb24b61980e
poonam-kumawat <mailpoonam2002@gmail.com>
2023-11-27 22:28:47 +0530

    added template

--- utils/genrateOTP.ts
+++ utils/genrateOTP.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import * as nodemailer from "nodemailer";
+import fs from "fs";
 
 dotenv.config();
 
@@ -11,23 +12,25 @@ let transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASSWORD,
   },
 });
+const emailTemplatePath =
+  "/home/poonam/Poonam/zest-backend/utils/template.html";
+const emailTemplate = fs.readFileSync(emailTemplatePath, "utf-8");
 
-const sendOTP = (email: string,otp:any): void => {
-  // otp=Math.floor(100000 + Math.random() * 900000).toString();
+const sendOTP = (email: string, otp: any): void => {
+  const emailBody = emailTemplate.replace("{{otp}}", otp);
   const mailOptions = {
     from: process.env.SMTP_MAIL,
     to: email,
-    subject: 'Your OTP Code',
-    html: `<p>Your OTP is: <strong>${otp}</strong></p>`,
+    subject: "Your OTP Code",
+    html: emailBody,
   };
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log('Error sending email: ' + error);
+      console.log("Error sending email: " + error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
     }
   });
 };
 
-
-export {sendOTP };
+export { sendOTP };
