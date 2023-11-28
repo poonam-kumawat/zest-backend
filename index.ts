124c26dbe052f97547cce1b8d55a0bc63d241fec
yashbhanu <bhanushaliyash2000@gmail.com>
2023-11-28 23:55:09 +0530

    changed html file path

--- index.ts
+++ index.ts
@@ -21,6 +21,7 @@ const allowedOrigins = ["http://localhost:3000"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
+app.use(express.static('public'))
 app.use(cors(options));
 app.use(express.json());
 app.use("/api/product", productRouter);
